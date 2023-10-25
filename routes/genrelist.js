3977df4f2c4afa217642994b37c68a9b9acab966
lelegoyeng <lelegoyengs@gmail.com>
2023-10-25 11:23:58 +0700

    fix delete routes genre

--- routes/genrelist.js
+++ routes/genrelist.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { MongoClient, ObjectId } = require("mongodb");
 
 module.exports = (connectToMongo) => {
     router.use(express.json());
