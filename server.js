869322281b4cdc08e9f99066ee8464e6ac63dc92
lelegoyeng <lelegoyengs@gmail.com>
2023-10-25 10:10:16 +0700

    update middleware

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
-require('dotenv').config()
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
 
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 menit
+    max: 100,
+});
+
+const allowedIPs = ['::ffff:192.168.0.44'];
+const restrictAccess = (req, res, next) => {
+    const clientIP = req.ip || req.connection.remoteAddress;
+    if (allowedIPs.includes(clientIP)) {
+        next();
+    } else {
+        res.status(403).json({ message: 'Akses Ditolak' });
+    }
+};
+
+app.use(helmet());
+app.use(express.json());
+app.use(restrictAccess);
+// app.use(limiter);
+
+
+
 const genrelistRoutes = require('./routes/genrelist');
-
 app.use('/genrelist', genrelistRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Server Anime berjalan di http://localhost:${PORT}`);
