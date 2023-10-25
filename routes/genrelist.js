eb88130197260a9183e8bef1cac0cd6ed8b9210d
lelegoyeng <lelegoyengs@gmail.com>
2023-10-25 10:42:21 +0700

    fix routes & connection mongodb

--- routes/genrelist.js
+++ routes/genrelist.js
@@ -1,75 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient, ObjectId } = require("mongodb");
-require('dotenv').config()
 
-const uri = process.env.mongodb
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+module.exports = (connectToMongo) => {
+    router.use(express.json());
 
-router.use(express.json());
-
-router.get('/', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("fuwafuwa");
-        const collection = database.collection("genrelist");
-
-        const result = await collection.find({}).toArray();
-        res.json(result);
-    } finally {
-        await client.close();
-    }
-});
-
-router.post('/', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("fuwafuwa");
-        const collection = database.collection("genrelist");
-
-        const get = await collection.find({}).toArray();
-        const genreId_Increment = Math.max(...get.map(value => value.genre_id));
-        const newGenre = {
-            nama: req.body.nama,
-            genre_id: genreId_Increment + 1
+    router.get('/', async (req, res) => {
+        try {
+            const database = await connectToMongo();
+            const collection = database.collection("genrelist");
+            const result = await collection.find({}).toArray();
+            res.json(result);
+        } catch (error) {
+            console.error('Error fetching genre list:', error);
         }
+    });
 
-        const result = await collection.insertOne(newGenre);
-        res.json(result);
-    } finally {
-        await client.close();
-    }
-});
+    router.post('/', async (req, res) => {
+        try {
+            const database = await connectToMongo();
+            const collection = database.collection("genrelist");
 
-router.put('/:id', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("fuwafuwa");
-        const collection = database.collection("genrelist");
+            const get = await collection.find({}).toArray();
+            const genreId_Increment = Math.max(...get.map(value => value.genre_id));
+            const newGenre = {
+                nama: req.body.nama,
+                genre_id: genreId_Increment + 1
+            }
 
-        const genreId = req.params.id;
-        const updatedGenre = req.body;
+            const result = await collection.insertOne(newGenre);
+            res.json(result);
+        } catch (error) {
+            console.error('Error adding genre:', error);
+        }
+    });
 
-        const result = await collection.updateOne({ _id: new ObjectId(genreId) }, { $set: updatedGenre });
-        res.json({ message: `${result.modifiedCount} document(s) updated` });
-    } finally {
-        await client.close();
-    }
-});
+    router.put('/:id', async (req, res) => {
+        try {
+            const database = await connectToMongo();
+            const collection = database.collection("genrelist");
 
-router.delete('/:id', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("fuwafuwa");
-        const collection = database.collection("genrelist");
+            const genreId = req.params.id;
+            const updatedGenre = req.body;
 
-        const genreId = req.params.id;
+            const result = await collection.updateOne({ _id: new ObjectId(genreId) }, { $set: updatedGenre });
+            res.json({ message: `${result.modifiedCount} document(s) updated` });
+        } catch (error) {
+            console.error('Error updating genre:', error);
+        }
+    });
 
-        const result = await collection.deleteOne({ _id: new ObjectId(genreId) });
-        res.json({ message: `${result.deletedCount} document(s) deleted` });
-    } finally {
-        await client.close();
-    }
-});
+    router.delete('/:id', async (req, res) => {
+        try {
+            const database = await connectToMongo();
+            const collection = database.collection("genrelist");
 
-module.exports = router;
+            const genreId = req.params.id;
+
+            const result = await collection.deleteOne({ _id: new ObjectId(genreId) });
+            res.json({ message: `${result.deletedCount} document(s) deleted` });
+        } catch (error) {
+            console.error('Error deleting genre:', error);
+        }
+    });
+
+    return router;
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors'); // Import cors
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const { MongoClient, ObjectId } = require("mongodb");
 require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
+
+const uri = process.env.mongodb;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 menit
@@ -13,6 +17,7 @@ const limiter = rateLimit({
 });
 
 const allowedIPs = ['::ffff:192.168.0.44'];
+
 const restrictAccess = (req, res, next) => {
     const clientIP = req.ip || req.connection.remoteAddress;
     if (allowedIPs.includes(clientIP)) {
@@ -22,15 +27,20 @@ const restrictAccess = (req, res, next) => {
     }
 };
 
+// Inisialisasi koneksi MongoDB
+const connectToMongo = async () => {
+    if (!client.connect()) {
+        await client.connect();
+    }
+    return client.db("fuwafuwa");
+};
+
 app.use(helmet());
 app.use(express.json());
 app.use(restrictAccess);
+app.use(cors());
 
-// Gunakan cors sebelum rute lainnya
-app.use(cors()); // Izinkan akses dari semua origin, bisa disesuaikan sesuai kebutuhan
-
-const genrelistRoutes = require('./routes/genrelist');
-app.use('/genrelist', genrelistRoutes);
+app.use('/genrelist', require('./routes/genrelist')(connectToMongo)); // Mengirim fungsi connectToMongo sebagai argumen ke route
 
 app.listen(PORT, () => {
     console.log(`Server Anime berjalan di http://localhost:${PORT}`);
