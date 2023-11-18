ca5f64d1660980e92888bc7175343d434fe1a62f
lelegoyeng <lelegoyengs@gmail.com>
2023-11-18 11:51:54 +0700

    Update

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +8,8 @@ const PORT = 5000;
 const routes = require("./routes/index");
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (_, res) => {
     res.send({ message: "Server Is Online. :)" });
